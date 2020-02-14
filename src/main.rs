b957026b2f077867b3c1bdf623290b0290137128
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2020-02-14 10:25:21 +0530

    Split into functions.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println! ("Welcome to the Guessing Game!");
     println! ("Please input a number between 1 and 100.");
 
     let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1,101);
-
+    let secret_number = generate_random_number();
     io::stdin().read_line(&mut guess).expect("Could not read the input.");
     let guess: u32 = guess.trim().parse().expect("Please enter a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println! ("You've guessed a little lower! The right answer is {}", secret_number),
-        Ordering::Greater => println! ("You've guessed a little more! The right answer is {}", secret_number),
-        Ordering::Equal => println!("You've guessed it right, it's {}!", secret_number)
+    let result: u32 = verify_guess_value(guess, secret_number);
+    if result == 1 {
+        println!("Sorry, you've guessed it a little less! The answer's {}", secret_number);
+    } else if result == 2 {
+        println!("Sorry, you've guessed it a little more! The answer's {}", secret_number);
+    } else {
+        println!("You're right! The secret is {}", secret_number);
+    }
+}
+
+fn generate_random_number() -> u32 {
+    let random_number = rand::thread_rng().gen_range(1,101);
+    return random_number;
+}
+
+fn verify_guess_value(user_guess: u32, secret_number: u32) -> u32 {
+    match user_guess.cmp(&secret_number) {
+        Ordering::Less => return 1,
+        Ordering::Greater => return 2,
+        Ordering::Equal => return 3
     }
 }
